5e900b7ae4aa562e219504a87792540643b05b77
fd <dadas>
2022-03-16 17:21:25 +0100

    Add project files.

---
+++ vj1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace vj1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
